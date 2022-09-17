2fbdb1e3b0c4c69d60b7f0125bdf979aad48c1a0
Luis Rivera <Siri@MacBook-Pro-de-whiz.local>
2022-09-17 02:52:56 -0500

    update on demo nft

--- DemoNFT.sol
+++ DemoNFT.sol
@@ -10,23 +10,13 @@ import './REF1699.sol';
 
 contract DemoNFT is Ownable, REF1699, ReentrancyGuard {
 
-    uint16 public immutable MAX_TEAM_SUPPLY = 150;
-    uint16 public teamCounter = 0;
-
     address private immutable CEO_ADDRESS = 0x8306865FAb8dEC66a1d9927d9ffC4298500cF7Ed;
     string public baseTokenURI;
 
     uint8 public saleStage; // 0: PAUSED | 1: SALE | 2: SOLDOUT
 
-    constructor() REF1699('Demo NFT', 'DEMONFT', 20, 3333, 1, 10) {
+    constructor() REF1699('Demo NFT', 'DEMONFT', 20, 4000, 1, 10) {
         saleStage = 0;
-    }
-
-    // UPDATE SALESTAGE
-
-    function setSaleStage(uint8 _saleStage) external onlyOwner {
-        require(saleStage != 2, "Cannot update if already reached soldout stage.");
-        saleStage = _saleStage;
     }
 
     // PUBLIC MINT 
@@ -36,7 +26,7 @@ contract DemoNFT is Ownable, REF1699, ReentrancyGuard {
         require(_quantity <= maxBatchSize, "Max mint at onece exceeded.");
         require(balanceOf(msg.sender) + _quantity <= 20, "Would reach max NFT per holder.");
         require(msg.value >= 0.015 ether * _quantity, "Not enough ETH.");
-        require(totalSupply() + _quantity + (MAX_TEAM_SUPPLY-teamCounter) <= collectionSize, "Mint would exceed max supply.");
+        require(totalSupply() + _quantity <= collectionSize, "Mint would exceed max supply.");
         require(msg.sender != referrer, "You cannot refer yourself.");
 
         // Implement the referrer royalty based on collection logic
@@ -46,17 +36,9 @@ contract DemoNFT is Ownable, REF1699, ReentrancyGuard {
 
         _safeMint(msg.sender, _quantity);
 
-        if (totalSupply() + (MAX_TEAM_SUPPLY-teamCounter) == collectionSize) {
+        if (totalSupply() == collectionSize) {
             saleStage = 2;
         }
-    }
-
-    // TEAM MINT
-
-    function teamMint(address _to, uint16 _quantity) external onlyOwner {
-        require(teamCounter + _quantity <= MAX_TEAM_SUPPLY, "Would exceed max team supply.");
-        _safeMint(_to, _quantity);
-        teamCounter += _quantity;
     }
     
     // METADATA URI
@@ -75,10 +57,4 @@ contract DemoNFT is Ownable, REF1699, ReentrancyGuard {
         return bytes(base).length > 0 ? string(abi.encodePacked(base, Strings.toString(tokenId), ".json")) : "";
     }
 
-    // WITHDRAW
-
-    function withdraw() external onlyOwner {
-        uint256 ethBalance = address(this).balance;
-        payable(CEO_ADDRESS).transfer(ethBalance);
-    }
 }
